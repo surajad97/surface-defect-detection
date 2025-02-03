684c8113075127d1976d436a5cf191b4f748c321
surajad97 <surajadhikari1997@gmail.com>
2025-02-03 01:55:24 +0100

    init commit

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ async def predict(file: UploadFile = File(...)):
 
 if __name__ == '__main__':
     uvicorn.run(app, host= '127.0.0.1', port= 8000)
-#uvicorn main:app --reload
+#uvicorn app:app --reload
